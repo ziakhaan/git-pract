18c47002d483b75316d74897a5806f2963f070e2
User <zia4mobileapp@gmail.com>
2022-06-11 17:06:12 +0530

    pushing new files

--- src/com/sp/training/Multiplier.java
+++ src/com/sp/training/Multiplier.java
@@ -10,4 +10,9 @@ public class Multiplier extends BinaryExpression {
 		return this.left.getValue() * this.right.getValue();
 	}
 
+	public void change1()
+	{
+		System.out.println("Chaange 1 added");
+	}
+
 }
